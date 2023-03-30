13041c35e95ba9e50997af01a95c986c5e81a2eb
Colonell <colonellj@hhmi.org>
2023-03-30 14:13:34 -0400

    update python version to add all metadata fields for SpikeGLX 032623 and later.

--- SGLXMetaToCoords.py
+++ SGLXMetaToCoords.py
@@ -13,7 +13,7 @@ The output is set by the outType parameter:
                   0 for text coordinate file; 
                   1 for Kilosort or Kilosort2 channel map file;
                   2 for strings to paste into JRClust .prm file 
-                  3 create a new metadata file with snsGeomMap appended (for converting old metadata to new)
+                  3 add fields to metadata that were missing prior to SpikeGLX version 032623-phase30
                   4 npy file with (nchan,2) matrix of xy coordinates, e.g. for YASS and related sorters                
 
 
@@ -163,6 +163,152 @@ def getGeomParams(meta):
 
 
 # =========================================================
+# Return full MUX table string to append to metadata
+#
+def getMuxTable(meta):
+# Read probe part number from meta
+# Return full MUX table string to append to metadata
+# As of 032923, there are 4 mux tables
+
+    np1=r'~muxTbl=(32,12)(0 1 24 25 48 49 72 73 96 97 120 121 144 145 168 169 192 193 216 217 240 241 264 265 288 289 312 313 336 337 360 361)(2 3 26 27 50 51 74 75 98 99 122 123 146 147 170 171 194 195 218 219 242 243 266 267 290 291 314 315 338 339 362 363)(4 5 28 29 52 53 76 77 100 101 124 125 148 149 172 173 196 197 220 221 244 245 268 269 292 293 316 317 340 341 364 365)(6 7 30 31 54 55 78 79 102 103 126 127 150 151 174 175 198 199 222 223 246 247 270 271 294 295 318 319 342 343 366 367)(8 9 32 33 56 57 80 81 104 105 128 129 152 153 176 177 200 201 224 225 248 249 272 273 296 297 320 321 344 345 368 369)(10 11 34 35 58 59 82 83 106 107 130 131 154 155 178 179 202 203 226 227 250 251 274 275 298 299 322 323 346 347 370 371)(12 13 36 37 60 61 84 85 108 109 132 133 156 157 180 181 204 205 228 229 252 253 276 277 300 301 324 325 348 349 372 373)(14 15 38 39 62 63 86 87 110 111 134 135 158 159 182 183 206 207 230 231 254 255 278 279 302 303 326 327 350 351 374 375)(16 17 40 41 64 65 88 89 112 113 136 137 160 161 184 185 208 209 232 233 256 257 280 281 304 305 328 329 352 353 376 377)(18 19 42 43 66 67 90 91 114 115 138 139 162 163 186 187 210 211 234 235 258 259 282 283 306 307 330 331 354 355 378 379)(20 21 44 45 68 69 92 93 116 117 140 141 164 165 188 189 212 213 236 237 260 261 284 285 308 309 332 333 356 357 380 381)(22 23 46 47 70 71 94 95 118 119 142 143 166 167 190 191 214 215 238 239 262 263 286 287 310 311 334 335 358 359 382 383)'
+    np2=r'~muxTbl=(24,16)(0 1 32 33 64 65 96 97 128 129 160 161 192 193 224 225 256 257 288 289 320 321 352 353)(2 3 34 35 66 67 98 99 130 131 162 163 194 195 226 227 258 259 290 291 322 323 354 355)(4 5 36 37 68 69 100 101 132 133 164 165 196 197 228 229 260 261 292 293 324 325 356 357)(6 7 38 39 70 71 102 103 134 135 166 167 198 199 230 231 262 263 294 295 326 327 358 359)(8 9 40 41 72 73 104 105 136 137 168 169 200 201 232 233 264 265 296 297 328 329 360 361)(10 11 42 43 74 75 106 107 138 139 170 171 202 203 234 235 266 267 298 299 330 331 362 363)(12 13 44 45 76 77 108 109 140 141 172 173 204 205 236 237 268 269 300 301 332 333 364 365)(14 15 46 47 78 79 110 111 142 143 174 175 206 207 238 239 270 271 302 303 334 335 366 367)(16 17 48 49 80 81 112 113 144 145 176 177 208 209 240 241 272 273 304 305 336 337 368 369)(18 19 50 51 82 83 114 115 146 147 178 179 210 211 242 243 274 275 306 307 338 339 370 371)(20 21 52 53 84 85 116 117 148 149 180 181 212 213 244 245 276 277 308 309 340 341 372 373)(22 23 54 55 86 87 118 119 150 151 182 183 214 215 246 247 278 279 310 311 342 343 374 375)(24 25 56 57 88 89 120 121 152 153 184 185 216 217 248 249 280 281 312 313 344 345 376 377)(26 27 58 59 90 91 122 123 154 155 186 187 218 219 250 251 282 283 314 315 346 347 378 379)(28 29 60 61 92 93 124 125 156 157 188 189 220 221 252 253 284 285 316 317 348 349 380 381)(30 31 62 63 94 95 126 127 158 159 190 191 222 223 254 255 286 287 318 319 350 351 382 383)'
+    np1100=r'~muxTbl=(32,12)(0 1 24 25 48 49 72 73 96 97 120 121 144 145 168 169 192 193 216 217 240 241 264 265 288 289 312 313 336 337 360 361)(2 3 26 27 50 51 74 75 98 99 122 123 146 147 170 171 194 195 218 219 242 243 266 267 290 291 314 315 338 339 362 363)(4 5 28 29 52 53 76 77 100 101 124 125 148 149 172 173 196 197 220 221 244 245 268 269 292 293 316 317 340 341 364 365)(6 7 30 31 54 55 78 79 102 103 126 127 150 151 174 175 198 199 222 223 246 247 270 271 294 295 318 319 342 343 366 367)(8 9 32 33 56 57 80 81 104 105 128 129 152 153 176 177 200 201 224 225 248 249 272 273 296 297 320 321 344 345 368 369)(10 11 34 35 58 59 82 83 106 107 130 131 154 155 178 179 202 203 226 227 250 251 274 275 298 299 322 323 346 347 370 371)(12 13 36 37 60 61 84 85 108 109 132 133 156 157 180 181 204 205 228 229 252 253 276 277 300 301 324 325 348 349 372 373)(14 15 38 39 62 63 86 87 110 111 134 135 158 159 182 183 206 207 230 231 254 255 278 279 302 303 326 327 350 351 374 375)(16 17 40 41 64 65 88 89 112 113 136 137 160 161 184 185 208 209 232 233 256 257 280 281 304 305 328 329 352 353 376 377)(18 19 42 43 66 67 90 91 114 115 138 139 162 163 186 187 210 211 234 235 258 259 282 283 306 307 330 331 354 355 378 379)(20 21 44 45 68 69 92 93 116 117 140 141 164 165 188 189 212 213 236 237 260 261 284 285 308 309 332 333 356 357 380 381)(22 23 46 47 70 71 94 95 118 119 142 143 166 167 190 191 214 215 238 239 262 263 286 287 310 311 334 335 358 359 382 383)'
+    np128ch=r'~muxTbl=(12,12)(84 11 85 5 74 10 56 112 46 121 39 127)(100 26 110 33 69 24 63 109 45 93 25 99)(87 0 82 6 71 15 53 117 43 122 42 116)(102 28 81 34 70 18 60 103 17 94 27 101)(73 1 86 7 68 16 50 106 40 123 128 128)(105 29 75 35 67 12 54 89 20 95 128 128)(76 2 83 8 65 13 47 118 49 124 128 128)(108 30 78 36 64 14 51 90 23 96 128 128)(79 3 80 9 62 114 44 119 52 125 128 128)(104 31 72 37 61 113 57 91 19 97 128 128)(88 4 77 21 59 111 41 120 55 126 128 128)(107 32 66 38 58 115 48 92 22 98 128 128)'
+       
+    M = dict([
+    ('3A',np1),
+    ('PRB_1_4_0480_1',np1),
+    ('PRB_1_4_0480_1_C',np1),
+    ('NP1010',np1), 
+    ('NP1011',np1),
+    ('NP1012',np1),
+    ('NP1013',np1),
+    
+    ('NP1015',np1),
+    ('NP1015',np1),
+    ('NP1016',np1),
+    ('NP1017',np1),
+       
+    ('NP1020',np1),
+    ('NP1021',np1),
+    ('NP1030',np1),
+    ('NP1031',np1),
+    
+    ('NP1022',np1),
+    ('NP1032',np1),
+    
+    ('NP1100',np1),
+    ('NP1110',np1100),
+    
+    ('PRB2_1_4_0480_1',np2),
+    ('PRB2_1_2_0640_0',np2),
+    ('NP2000',np2),
+    ('NP2003',np2),
+    ('NP2004',np2),
+    
+    ('PRB2_4_2_0640_0',np2),
+    ('PRB2_4_4_0480_1',np2),
+    ('NP2010',np2),
+    ('NP2013',np2),
+    ('NP2014',np2),
+    
+    ('NP1120',np1),
+    ('NP1121',np1),
+    ('NP1122',np1),
+    ('NP1123',np1),
+    ('NP1300',np1),
+    
+    ('NP1200',np128ch),
+    ('NXT3000',np128ch)
+    ])
+    
+    # get probe part number; if absent, this is a 3A
+    if 'imDatPrb_pn' in meta:   
+        pn = meta['imDatPrb_pn']
+    else:
+        pn = '3A';
+
+
+    if pn in M:
+        muxTableStr = M[pn] + '\n'
+    else:
+        print('unsupported probe part number\n')
+        muxTableStr = []
+    
+    return muxTableStr
+
+
+# =========================================================
+# Parse imro table to extract 'new' metadata items
+# (SpikeGLX 032623-phase 30 and later)
+# Returns strings for:
+#     imChan0apGain
+#     imChan0lfGain
+#     imAnyChanFullBand
+# 
+#
+def imroMetaItems(meta):
+        
+    # read in the imro table 
+    imroTbl = meta['imroTbl'].split(sep=')')
+    
+    # there is an entry in the map for each saved channel
+    # number of entries in map -- subtract 1 for header, one for trailing ')'
+    nEntry = len(imroTbl) - 2
+
+    # read header to get what type of imro this is
+    headEntry = imroTbl[0]
+    headEntry = headEntry[1:len(headEntry)]   #remove leading ')'
+    headList = headEntry.split(sep=',')
+    currType = headList[0]
+    
+    if int(currType) > 50000:
+        # this is a 3A probe
+        currType = '0'
+
+    if currType == '24' or currType == '21':
+        imChan0apGainStr = 'imChan0apGain=80'
+        imChan0lfGainStr = 'imChan0lfGain=80'
+        imAnyChanFullBandStr = 'imAnyChanFullBand=true'
+        
+    elif currType == '0':
+        # parse first entry of the table for lf and apgain
+        currEntry = imroTbl[1]
+        currEntry = currEntry[1:len(currEntry)]   #remove leading '())'
+        currList = currEntry.split(sep=' ')
+        imChan0apGainStr = 'imChan0apGain=' + currList[3]
+        imChan0lfGainStr = 'imChan0lfGain=' + currList[4]
+                   
+        imAnyChanFullBandStr = 'imAnyChanFullBand=false'
+        if len(currList) == 6:  # indicates this is not a 3A imro table
+        #check for any full band
+            for i in range(nEntry):
+                # check for any channels where the AP filter was not used
+                currEntry = imroTbl[i+1]
+                currEntry = currEntry[1:len(currEntry)]
+                currList = currEntry.split(sep=' ')
+                if currList[5] == '0':
+                    imAnyChanFullBandStr = 'imAnyChanFullBand=true'
+                    break
+
+    elif currType == '1110':
+        # ap and lf gain and filter option are in the header 
+        imChan0apGainStr = 'imChan0apGain=' + headList[3]
+        imChan0lfGainStr = 'imChan0lfGain=' + headList[4]
+        imAnyChanFullBandStr = 'imAnyChanFullBand=false'
+        if headList[5] == 0:
+            imAnyChanFullBandStr = 'imAnyChanFullBand=true'
+   
+    
+    # add newline characters to each
+    imChan0apGainStr = imChan0apGainStr + '\n'
+    imChan0lfGainStr = imChan0lfGainStr + '\n'
+    imAnyChanFullBandStr = imAnyChanFullBandStr + '\n'
+    
+    return imChan0apGainStr, imChan0lfGainStr, imAnyChanFullBandStr
+
+
+# =========================================================
 # Parse snsGeomMap for XY coordinates
 #
 def geomMapToGeom(meta):
@@ -203,7 +349,12 @@ def geomMapToGeom(meta):
 #
 def snsGeom(meta, shankInd, xCoord, yCoord, use):
     # header
-    pn = meta['imDatPrb_pn']
+    # get probe part number; if absent, this is a 3A
+    if 'imDatPrb_pn' in meta:   
+        pn = meta['imDatPrb_pn']
+    else:
+        pn = '3A';
+        
     geomList = getGeomParams(meta)
     # geomList =
     # [nShank, shankWidth, shankPitch, even_xOff, odd_xOff, horizPitch, vertPitch, rowsPerShank, elecPerShank]
@@ -214,6 +365,8 @@ def snsGeom(meta, shankInd, xCoord, yCoord, use):
     
     for i in range(0,nEntry):
         snsGeomStr = snsGeomStr + '({:g}:{:g}:{:g}:{:g})'.format(shankInd[i], xCoord[i], yCoord[i], use[i])
+    
+    snsGeomStr = snsGeomStr + '\n'
     
     return snsGeomStr
 
@@ -430,19 +583,29 @@ def CoordsToKSChanMap(meta, chans, xCoord, yCoord, connected, shankInd, shankSep
 def CoordsToGeomMap(meta, chans, xCoord, yCoord, connected, shankInd, shankSep, baseName, savePath, buildPath ):
 
     if buildPath:
-        newName = baseName + '_plusGeom.meta'
-        saveFullPath = Path(savePath / newName)
+        newName = baseName + '_orig.meta'
+        copyFullPath = Path(savePath / newName)
     else:
         print('Can only make new metadata in same directory as original.')
         return
     
     origPath = Path(savePath / (baseName + '.meta'))
-    shutil.copy(origPath, saveFullPath)
+    shutil.move(origPath, copyFullPath)
+    shutil.copy(copyFullPath,origPath)
     
-    snsGeomStr = snsGeom(meta, shankInd, xCoord, yCoord, connected)
+    # check 'new' fields; add if not present, add everything (most common case)
+    if 'imChan0apGain' not in meta:        
+        imChan0apGainStr, imChan0lfGainStr, imAnyChanFullBandStr = imroMetaItems(meta)
+        muxTableStr = getMuxTable(meta)        
+        snsGeomStr = snsGeom(meta, shankInd, xCoord, yCoord, connected)
     
-    with open(saveFullPath, 'a') as outFile:
-        outFile.write(snsGeomStr)
+        with open(origPath, 'a') as outFile:
+            outFile.write(imChan0apGainStr)
+            outFile.write(imChan0lfGainStr)
+            outFile.write(imAnyChanFullBandStr)
+            outFile.write(muxTableStr)
+            outFile.write(snsGeomStr)
+            
 
 
 # =========================================================
